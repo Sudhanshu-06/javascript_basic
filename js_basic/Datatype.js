83ee7d7207f8bb4679e8e7650d7f6856a019f419
Sudhanshu Kumar <114154455+Sudhanshu-06@users.noreply.github.com>
2023-10-20 02:22:49 +0000

    done

---
+++ js_basic/Datatype.js
@@ -0,0 +1,19 @@
+"use strict;" //treat all js code as a new version
+
+// alert(3+3) // we are using node js not browser
+
+console.log("Sudhanshu")
+
+let name="Sudhanshu"
+let age=24
+let isLoggedIn=true
+// num=> 2 to power 53
+// bigint
+// string=>""
+// boolean= true/false
+// null= standalone value
+// undefined=>
+//symbol=>unique
+
+// object
+console.log(typeof "Sudhanshu");
